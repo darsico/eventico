153e73c4d4a3910cac7a4e50778d65540a47d110
Diego Huaman <diegojairhl@gmail.com>
2024-09-27 10:59:32 -0500

    add: update events

--- routes/events.go
+++ routes/events.go
@@ -60,3 +60,42 @@ func createEvent(context *gin.Context) {
     "event": event,
   })
 }
+
+func updateEvent (context *gin.Context) {
+  rawEventId := context.Param("id")
+  eventId, err := strconv.ParseInt(rawEventId, 10, 64)
+
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID", "message": err.Error()})
+    return
+  }
+
+  _, err = models.GetEventById(eventId)
+
+  if err != nil {
+    context.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting event", "message": err.Error()})
+    return
+  }
+
+  var updatedEvent models.Event
+  
+  err = context.ShouldBindJSON(&updatedEvent)
+
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data", "message": err.Error()})
+    return
+  }
+
+  updatedEvent.ID = eventId
+
+  err = updatedEvent.Update()
+
+  if err != nil {
+    context.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating event", "message": err.Error()})
+    return
+  }
+
+  context.JSON(http.StatusOK, gin.H{
+    "message": "Event updated successfully!",
+  })
+}
