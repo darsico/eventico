f1db8e25ad26f24b35c9ed34f6f49d8595c598cb
Diego Huaman <diegojairhl@gmail.com>
2024-09-27 11:38:40 -0500

    create users table and reference

--- db/db.go
+++ db/db.go
@@ -24,6 +24,20 @@ func InitDB() {
 }
 
 func createTables() {
+  createUserTable := `
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `
+
+  _, err := DB.Exec(createUserTable)
+
+  if err != nil {
+    panic(fmt.Sprintf("Could not create users table: %v", err))
+  }
+
   createEventTable := `
     CREATE TABLE IF NOT EXISTS events (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,10 +46,11 @@ func createTables() {
       location TEXT NOT NULL,
       dateTime DATETIME NOT NULL,
       user_id INTEGER
+      FOREIGN KEY(user_id) REFERENCES users(id)
     );
   `
 
-  _, err := DB.Exec(createEventTable)
+  _, err = DB.Exec(createEventTable)
   if err != nil {
     panic(fmt.Sprintf("Could not create events table: %v", err))
   }
