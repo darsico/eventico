f68e346d4d01f352a5d604a2204e99a76ae887b0
Diego Huaman <diegojairhl@gmail.com>
2024-09-27 15:14:05 -0500

    assign userId to event

--- models/events.go
+++ models/events.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Event struct {
-  ID int64
-  Name string `binding:"required"`
-  Description string `binding:"required"`
-  Location string `binding:"required"`
-  DateTime time.Time `binding:"required"`
-  UserID int
+	ID          int64     `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"datetime" binding:"required"`
+	UserID      int64     `json:"user_id"`
 }
 
-func (e Event) Save() error {
+func (e* Event) Save() error {
   query := `
   INSERT INTO events(name, description, location, dateTime, user_id)
   VALUES (?, ?, ?, ?, ?)`
